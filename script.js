12d16c9a475bde032f4728d0a09e79aceeccc3a9
ericameyaw <106287746+Jumpman2324@users.noreply.github.com>
2023-08-16 14:59:43 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const ratingEls = document.querySelectorAll(".rating")
+const btnEl = document.getElementById("btn")
+const containerEl = document.getElementById("container")
+
+let selectedRating = "";
+
+
+ratingEls.forEach ((ratingEl) => {
+    ratingEl.addEventListener("click",(event) => {
+        removeActive()
+        selectedRating = event.target.textContent || event.target.parentNode.textContent;
+        event.target.classList.add("active")
+        event.target.parentNode.classList.add("active")
+    })
+})
+
+btnEl.addEventListener("click",() =>{
+    if (selectedRating !== "") {
+        containerEl.innerHTML = ` <strong>Thank you! </strong>
+        <br>
+        <br>
+        <strong>Feedback: ${selectedRating} </strong>
+        <strong> <p>Well use your feedback to improve our customer support </p>    </strong>
+        
+
+
+         `
+    }
+})
+
+function removeActive() {
+    ratingEls.forEach((ratingEl) =>{
+        ratingEl.classList.remove("active")
+    })
+}
